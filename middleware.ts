238692b2ddbd5a99124ae86118a864c6d7c6411b
antryu <seongpilryu@gmail.com>
2025-11-23 14:09:37 +0900

    Temporarily disable authentication for development

--- middleware.ts
+++ middleware.ts
@@ -9,6 +9,10 @@ export async function middleware(req: NextRequest) {
     },
   })
 
+  // DEVELOPMENT ONLY: Skip authentication checks
+  // TODO: Remove this in production!
+  return res
+
   // Skip middleware if environment variables are not set
   if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
     return res
